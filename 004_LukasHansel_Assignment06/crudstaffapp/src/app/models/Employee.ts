6ea49f02cd4de757fcd1b72b40bcfb9782df104f
lukaslynch <lukaslynch98@gmail.com>
2021-11-02 23:33:35 +0700

    Angular session

---
+++ 004_LukasHansel_Assignment06/crudstaffapp/src/app/models/Employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    id: number,
+    title: string,
+    firstName: string,
+    lastName: string,
+    role: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+}
